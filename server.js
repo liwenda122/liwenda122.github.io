fd3beff7db3f5130e9506876641e35e40b70c92b
Wenda <382430560@qq.com>
2022-11-07 19:02:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,102 @@
-const Webpack = require("webpack")
-const WebpackDevServer = require('webpack-dev-server')
-const webpackConfig = require("./webpack.config")
-const config = require('./config/')
-var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-
-// for(val in webpackConfig.entry){
-//     if(!(webpackConfig.entry[val] instanceof Array)){
-//         var obj = new Array();
-//         obj.push(webpackConfig.entry[val]);
-//         webpackConfig.entry[val]=obj;
-//     }
-//     webpackConfig.entry[val].push("webpack-dev-server/client?http://localhost:81");
-//     webpackConfig.entry[val].push("webpack/hot/dev-server");
-//     console.log("######添加模块："+val+"热加载成功######")
-// }
-webpackConfig.plugins.push(new Webpack.HotModuleReplacementPlugin());
-var compiler = Webpack(webpackConfig);
-var server = new WebpackDevServer(compiler, {
-    publicPath: "/app/",
-    stats: {
-        colors: true //显示不同的颜色区分打包的文件
+// pie 环形图
+option = {
+    backgroundColor: '#fff',
+    title: [{
+        text: '95%',
+        textStyle: {
+            color: '#22ac38',
+            fontSize: 35
+        },
+        itemGap: 20,
+        left: '46%',
+        top: '40%'
+    },{
+        text: '总体完成度',
+        textStyle: {
+            color: '#444444',
+            fontSize: 35,
+            fontWeight:'normal'
+        },
+        itemGap: 20,
+        left: '41.5%',
+        top: '50%'
+    }],
+    grid: [{
+        top: '10%',
+        width: '50%',
+        left: '45%',
+        containLabel: true
+    }],
+    angleAxis: {
+        polarIndex: 0,
+        min: 0,
+        max: 100,
+        show: false,
+        // boundaryGap: ['40%', '40%'],
+        startAngle: 90,
     },
-    proxy: { //代理服务器
-        '/pms/*': 'http://192.168.1.64:8080'
-    },
-    hot:true,
-    inline:true,
-    progress:true,
-    historyApiFallback:true,
-})
+    radiusAxis: {
+        type: 'category',
+        show: true,
+        axisLabel: {
+            show: false,
+        },
+        axisLine: {
+            show: false,
 
-server.listen(3001, (err) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log('http://localhost:3001' + config.publicPath)
-})
+        },
+        axisTick: {
+            show: false
+        },
+    },
+    polar: [{
+        center: ['50%', '50%'], //中心点位置
+        radius: '80%' //图形大小
+    }],
+    series: [
+        {
+            type: 'bar',
+            z: 10,
+            name: "完成度",
+            data: [95],
+            showBackground: false,
+            backgroundStyle: {
+                borderWidth: 10,
+                width: 10
+            },
+            coordinateSystem: 'polar',
+            roundCap: true,
+            barWidth: 35, //大的占比环
+            itemStyle: {
+                normal: {
+                    opacity: 1,
+                    color: "#14d88e"
+                }
+            },
+        },
+        {
+            type: 'pie',
+            name: '内层细圆环',
+            radius: ['46%', '34%'],
+            center: ['50%', '50%'], //中心点位置
+            startAngle: 90,
+            hoverAnimation: false,
+            clockWise: true,
+             silent: true,
+            itemStyle: {
+                normal: {
+                    color: '#f3f3f7',
+                    shadowBlur: 0,
+                    shadowColor: '#66666a',
+                }
+            },
+            tooltip: {
+                show: false,
+            },
+            label: {
+                show: false
+            },
+            data: [100]
+        }
+    ]
+};
