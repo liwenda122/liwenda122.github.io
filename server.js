cdd25a9d557991c32d33f9167009384a1ad3ff71
Wenda <382430560@qq.com>
2022-11-07 19:28:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -100,3 +100,86 @@ option = {
         }
     ]
 };
+// 柱状图 条形百分比
+option = {
+    grid: {
+        left: '5%',
+        right: '5%',
+        bottom: '5%',
+        top: '10%',
+        containLabel: true
+    },
+    backgroundColor: '#ffffff',
+    xAxis: {
+        show: false,
+        type: 'value'
+    },
+    yAxis: [{
+        type: 'category',
+        inverse: true,
+        axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#303133',
+                fontSize: '14'
+            },
+        },
+        splitLine: {
+            show: false
+        },
+        axisTick: {
+            show: false
+        },
+        axisLine: {
+            show: false
+        },
+        data: ['客车', '重工', '商用车', '专用车']
+    }, {
+        type: 'category',
+        inverse: true,
+        axisTick: 'none',
+        axisLine: 'none',
+        show: true,
+        axisLabel: {
+            textStyle: {
+                color: '#303133',
+                fontSize: '14'
+            },
+            formatter: function(value) {
+                if (value >= 10000) {
+                    return (value / 10000).toLocaleString() + '万';
+                } else {
+                    return value.toLocaleString() + '%'
+                }
+            },
+        },
+        data: [23, 18, 12, 6]
+    }],
+    series: [{
+            name: '次数',
+            type: 'bar',
+            zlevel: 1,
+            itemStyle: {
+                normal: {
+                    barBorderRadius: 0,
+                    color: '#419fff'
+                },
+            },
+            // barWidth: 8,
+            data: [23, 18, 12, 6]
+        },
+        {
+            name: '背景',
+            type: 'bar',
+            // barWidth: 8,
+            barGap: '-100%',
+            data: [23, 23, 23, 23],
+            itemStyle: {
+                normal: {
+                    color: '#E4E7ED',
+                    barBorderRadius: 0,
+                }
+            },
+        },
+    ]
+};
