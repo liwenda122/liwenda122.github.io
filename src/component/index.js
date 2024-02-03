4b8548212a11fdef68a1546c4834c5190c26f479
Wenda <382430560@qq.com>
2024-02-03 18:23:14 +0800

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -5,3 +5,4 @@ import layout from './layout'
 export default { header,navmenu,breadcrumb,layout }
 http://disk.yutong.com:10060/v/link/view/FSgzWdefdea6a754548ea884addc1de6c4c9a
 http://disk.yutong.com:10060/v/link/view/ZblSc05a4a3f63d514b9eb0603e4f7a846072
+http://disk.yutong.com:10060/v/link/view/Ipm3sb1ce3c5a6e464889875108248ac5b66d
