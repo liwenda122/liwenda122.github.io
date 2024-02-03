b7b192e0d421e3f2e0177b187670e921df606d05
Wenda <382430560@qq.com>
2024-02-03 18:07:26 +0800

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -4,3 +4,4 @@ import breadcrumb from './breadcrumb'
 import layout from './layout'
 export default { header,navmenu,breadcrumb,layout }
 http://disk.yutong.com:10060/v/link/view/FSgzWdefdea6a754548ea884addc1de6c4c9a
+http://disk.yutong.com:10060/v/link/view/ZblSc05a4a3f63d514b9eb0603e4f7a846072
